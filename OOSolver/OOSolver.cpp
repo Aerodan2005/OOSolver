1cc6639cbc2cdf678f8ebdd6b57fa4a6d9029d0d
Omris <Omris@LAPTOP-Q5VFVPN8>
2022-09-28 13:50:23 +0300

    bu

--- OOSolver/OOSolver.cpp
+++ OOSolver/OOSolver.cpp
@@ -4,11 +4,20 @@
 #include <iostream>
 #include "BaseSolver.h"
 #include "Method1Solver.h"
+#include "Method2Solver.h"
 
 int main(int argc, char** argv)
 {
   std::cout << "OO Solver, received: " << argc << " arguments\n";
+  // read the data
+  // solve method1
   BaseSolver * bs = new Method1Solver ();
   bs->InitProblem(5, nullptr, nullptr);
   bs->Solve();
+  delete bs;
+  // solve method2
+  bs = new Method2Solver();
+  bs->InitProblem(5, nullptr, nullptr);
+  bs->Solve();
+  delete bs;
 }
